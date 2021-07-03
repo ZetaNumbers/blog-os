8c92b5a568b94dc42ca79bbeb58fd0915bbac79b
zetanumbers <zeta0.114942@gmail.com>
2021-07-03 18:27:50 +0300

    Removed `no_panic` due to `panic = abort`

--- src/panic.rs
+++ src/panic.rs
@@ -1,15 +1,12 @@
 use crate::{qemu, serial_println};
-use no_panic::no_panic;
 
 #[allow(unreachable_code)]
-#[no_panic]
 pub fn vga_panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
-    crate::println!("{}", panic_info);
+    crate::print!("{}", panic_info);
     loop {}
 }
 
 #[allow(unreachable_code)]
-#[no_panic]
 pub fn test_panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
     use crate::{qemu, serial_println};
     serial_println!("[failed]\n");
@@ -18,7 +15,6 @@ pub fn test_panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
 }
 
 #[allow(unreachable_code)]
-#[no_panic]
 pub fn fail_test_panic_handler(_: &core::panic::PanicInfo) -> ! {
     serial_println!("[ok]");
     qemu::exit(qemu::ExitCode::Success);
